1c7950c6a988cb770f8f84de0378742328abe3ec
basalingayyakaramadi <praveenkaramadi90@gmail.com>
2024-06-13 12:06:20 +0530

    initial test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppsComponent } from './apps/apps.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'apps',
+    component: AppsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
